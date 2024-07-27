01bb5caf68e8b10fddbcda439d3d4c7e7f210179
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2024-07-26 22:04:20 -0300

    Update variaveis.php

---
+++ web-completo/section013/aula315/variaveis.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $nome = 'Erick Santos';
+        $idade = 29;
+        $peso = 82.5;
+        $fumante = false;
+    ?>
+
+    <h1>Ficha cadastral</h1>
+    <br>
+    <p>Nome: <?php $nome ?></p>
+    <p>Idade: <?php $idade ?></p>
+    <p>Peso: <?php $peso ?></p>
+    <p>Fumante: <?php $fumante ?></p>
+</body>
+</html>
