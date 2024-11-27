79433f97b92856b3a19832cd2d5a40455d5b8ee0
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2024-11-27 20:13:15 -0300

    Create funcoes-arrays.php

---
+++ web-completo/section013/aula347/funcoes-arrays.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $array = 'String';
+        $retorno = is_array($array);
+
+        if ($retorno) {
+            echo 'Sim, é um array';
+        } else {
+            echo 'Não, não é um array';
+        }
+        
+
+    ?>
+</body>
+</html>
