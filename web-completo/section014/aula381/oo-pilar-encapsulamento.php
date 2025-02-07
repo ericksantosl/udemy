a9208b4c648f1c15c3e904f9d9c7b0571ab2a965
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2025-02-06 21:34:32 -0300

    Create oo-pilar-encapsulamento.php

---
+++ web-completo/section014/aula381/oo-pilar-encapsulamento.php
@@ -0,0 +1,43 @@
+<?php
+    class Pai {
+        private $nome = 'Jorge';
+        protected $sobrenome = 'Silva';
+        public $humor = 'Feliz';
+        
+        public function __get($attr) {
+            return $this->$attr;
+        }
+
+        public function __set($attr, $value) {
+            $this->$attr = $value;
+        }
+        
+
+        private function executarMania() {
+            echo 'Assobiar';
+        }
+
+        protected function responder() {
+            echo 'Oi';
+        }
+
+        public function executarAcao() {
+            $x = rand(1, 10);
+
+            if ($x >= 1 && $x <= 5) {
+                $this->executarMania();
+            } else {
+                $this->responder();
+            }
+
+            
+        }
+    }
+
+    class Filho extends Pai {
+        
+    }
+
+    $filho = new Filho();
+    echo $filho->getAtributo('sobrenome');
+?>
