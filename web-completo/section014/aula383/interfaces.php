0cb4b0463e012581cdcfca4a726ec6735ebdd220
ericksantosl <156433773+ericksantosl@users.noreply.github.com>
2025-02-11 21:19:10 -0300

    Create interfaces.php

---
+++ web-completo/section014/aula383/interfaces.php
@@ -0,0 +1,35 @@
+<?php
+    //interface
+    interface EquipamentoEletronicoInterface {
+        public function ligar();
+        public function desligar();
+    }
+
+    class Geladeira implements EquipamentoEletronicoInterface{
+        public function abrirPorta() {
+            echo 'Abrir porta';
+        }
+
+        public function ligar() {
+            echo 'Ligar';
+        }
+
+        public function desligar() {
+            echo 'Desligar';
+        }
+    }
+
+    class TV implements EquipamentoEletronicoInterface {
+        public function trocarCanal() {
+            echo 'Trocar canal';
+        }
+
+        public function ligar() {
+            echo 'Ligar';
+        }
+
+        public function desligar() {
+            echo 'Desligar';
+        }
+    }
+?>
